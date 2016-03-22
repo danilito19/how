ad5fc688510d86837c07ffcbb736254c1ae612e9
danilito19 <danalitovsky@gmail.com>
2016-03-22 14:24:30 -0500

    add js for button

---
+++ static/js/index.js
@@ -0,0 +1,12 @@
+$('#button').click(function(e){
+    e.preventDefault();
+	var home = document.getElementById("home_zip");
+
+
+
+	console.log(home);
+
+	// document.getElementById('priorities').value = priority_list;
+	// $('#user-form').attr('action', "./").submit();
+});
+
