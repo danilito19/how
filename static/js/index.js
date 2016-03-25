de48f4908086bbfc5ff409200d1543f871931729
danilito19 <danalitovsky@gmail.com>
2016-03-25 09:16:37 -0500

    clean up js and change radio choices

--- static/js/index.js
+++ static/js/index.js
@@ -1,27 +1,6 @@
-// $('button').click(function(e){
-//     e.preventDefault();
-
-
-
-// 	console.log(home);
-
-// 	// document.getElementById('priorities').value = priority_list;
-// 	// $('#priorities-form').attr('action', "./preferences_citysize/").submit();
-// });
-// console.log('loaded index.js');
-// var myButton = document.querySelector('button');
-// myButton.onclick = function(e) {
-// 	e.preventDefault();
-//   console.log('hello');
-// }
-
 $( "form" ).submit(function(e) {
   e.preventDefault();
   var userInputs = $(this).serializeArray();
-  // [{name: 'homezip', value: 75989}, {name: 'workzip', value: 83729}]
-
-  // userInputs.map(function(input){return input.values;})
-  // this returns only values: [38923, 839483, 238]
 
  // transform to dict
   var dict = {};
@@ -31,17 +10,6 @@ $( "form" ).submit(function(e) {
   }
   userInputs.forEach(toDict);
 
-  // function dictToString(inputDict) {
-  // 	var outputString = '';
-  // 	Object.keys(inputDict).forEach(function(key){
-  // 		outputString = outputString + inputDict[key] + ' ';
-  // 	});
-  // 	return outputString;
-  // }
-
-  console.log('about to send ajax request');
   console.log(dict);
-  $.getJSON('/results', dict, function (data) {
-  	console.log('got back the ajax request: ', data);
-	});
+  $.getJSON('/results', dict);
 });
